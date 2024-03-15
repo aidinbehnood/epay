24148d51cb2cfb9235a8a17cea4dd2bbff7a5073
aidin <aidin.behnood@yahoo.com>
2024-03-15 17:22:13 +0100

    final

--- epay.Server/Utils/Extension.cs
+++ epay.Server/Utils/Extension.cs
@@ -10,25 +10,7 @@ namespace epay.Server.Utils
         public static bool IsValidAge(this int age)
     => age > 18 ;
 
-        public static bool CheckDigitExpireDate(this string expireDate)
-        {
-            var digit = int.Parse(expireDate.Substring(2, 2));
-            if (digit >= 1 && digit <= 12)
-                return true;
-            return false;
-        }
-        //public static bool CheckDuplicateId(this int id)
-        //{
-            
-        //    //var digit = int.Parse(expireDate.Substring(2, 2));
-        //    //if (digit >= 1 && digit <= 12)
-        //    //    return true;
-        //    return false;
-        //}
-
-
+    
         
-        public static bool IsValidStan(this int stan)
-      => !(stan < 1 || stan > 999999);
     }
 }
