62f1d36ec8621870d82e36565852fa912737f2d7
aidin <aidin.behnood@yahoo.com>
2024-03-16 07:47:42 +0100

    last update

--- epay.Server/data/DataContext.cs
+++ epay.Server/data/DataContext.cs
@@ -7,7 +7,7 @@ namespace epay.Server.data
 {
     public static class DataContext
     {
-        
+
         public static async Task<List<Customer>> ReadData()
         {
             List<Customer> customers = new List<Customer>();
@@ -31,17 +31,48 @@ namespace epay.Server.data
             }
 
             oldCustmerlst.AddRange(customers);
-            var newCustomerlst =oldCustmerlst.OrderBy(x => x.lastName).ThenBy(x => x.firstName).ToList();
-            string json2 = JsonConvert.SerializeObject(newCustomerlst);
+            BubblesortBy(x => x.lastName, oldCustmerlst);
+            BubblesortBy(x => x.firstName, oldCustmerlst);
+            //var newCustomerlst = oldCustmerlst.OrderBy(x => x.lastName).ThenBy(x => x.firstName).ToList();
+            string json2 = JsonConvert.SerializeObject(oldCustmerlst);
             File.WriteAllText(@"data/Customers.json", json2);
-           
+
         }
 
         public static async Task<bool> CheckDuplicateID(int id)
         {
-            var customerIds= ReadData().Result.Select(x=>x.id).ToList();
+            var customerIds = ReadData().Result.Select(x => x.id).ToList();
             return customerIds.Any(x => x == id);
 
+        }
+
+
+        public static void BubblesortBy<TSource, TKey>(Func<TSource, TKey> keySelector,
+                                        List<TSource> stocks)
+        {
+            int loopCount = 0;
+            bool doBreak = true;
+
+            for (int i = 0; i < stocks.Count; i++)
+            {
+                doBreak = true;
+                for (int j = 0; j < stocks.Count - 1; j++)
+                {
+                    if (Compare(keySelector(stocks[j]), keySelector(stocks[j + 1])))
+                    {
+                        TSource temp = stocks[j + 1];
+                        stocks[j + 1] = stocks[j];
+                        stocks[j] = temp;
+                        doBreak = false;
+                    }
+                    loopCount++;
+                }
+                if (doBreak) { break;  }
+            }
+        }
+        private static bool Compare<T>(T l, T r)
+        {
+            return Comparer<T>.Default.Compare(l, r) > 0;
         }
     }
 }
